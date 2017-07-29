c96fa2820aa7c685853ccb6aa87b3d617f01b114
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2017-07-29 14:14:16 -0500

    Moves section content in component state, and iterates sections with map

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import { Grid, Row, Col } from 'react-flexbox-grid';
 import styled from 'styled-components';
 
 let Section = styled.section`
-	background: ${props => props.bgColor};
+	background-color: ${props => props.bgColor};
 	color: ${props => props.textColor};
 	min-height: 100vh;
 	border-top: 1px solid white;
@@ -42,63 +42,49 @@ let Image = styled.img`
 `;
 
 class HomePage extends React.Component {
+	constructor() {
+		super();
+
+		 const content = [
+		    {
+		      title: `Shrumen Lumen`,
+		      body: `Interactive art installation made up of five glowing mushrooms
+						that react to the presence of people. Presented at Burning Man 2016
+						and Meet D3 Festival in Dubai. On diplay at the Smithsonian Museum
+						in 2018.`,
+		      roles: `Lead Software Developer (First Showings),
+						Hardware-Software Integration.`,
+		      media: `img/shrumenlumen/Shrumen-Playa-day-50.jpg`,
+		      bgColor: `#919FB4`,
+		      textColor: `#FFF`
+		    },
+		    {
+		      title: `Dial Up`,
+		      body: `Creative collective that creates music, videos, magazines, and
+						technology. Website featured on Brutalist Websites.`,
+		      roles: `Website Design/Develoment, DJ, Writer.`,
+		      media: `img/dialup/dialupscreen.png`,
+		      bgColor: `#FFF`,
+		      textColor: `#FF433E`
+		    }
+		  ];
+
+			this.state = {
+				content: content,
+			};
+	}
   render() {
 		return (
 			<div>
-				<Section
-					bgColor="black"
-					textColor="white"
-				>
+				<Section bgColor="black" textColor="white">
 					<div id="three"></div>
 					<Tagline>software. design. art.</Tagline>
 				</Section>
-				<Section
-					bgColor="#919FB4"
-					textColor="white"
-				>
-					<Grid fluid>
-						<Row>
-							<Col lg={4}>
-								<Title>SHRUMEN LUMEN</Title>
-								<Body>
-									Interactive art installation made up of five glowing mushrooms
-									that react to the presence of people. Presented at Burning Man
-									 ’16 and Meet D3 Festival in Dubai. On diplay at the
-									 Smithsonian Museum in 2018.
-								</Body>
-								<Role>
-									Roles: Lead Software Developer (First Showings),
-									Hardware-Software Integration.
-								</Role>
-							</Col>
-							<Col lgOffset={1} lg={7}>
-								<Image src="img/shrumenlumen/Shrumen-Playa-day-50.jpg" />
-							</Col>
-						</Row>
-					</Grid>
-				</Section>
-				<Section
-					bgColor="white"
-					textColor="#FF433E"
-				>
-					<Grid fluid>
-						<Row>
-							<Col lg={4}>
-								<Title>DIAL UP</Title>
-								<Body>
-									Creative collective that creates music, videos, magazines, and
-									 technology. Website featured on Brutalist Websites.
-								</Body>
-								<Role>
-									Roles: Website Design/Develoment, DJ, Writer.
-								</Role>
-							</Col>
-							<Col lgOffset={1} lg={7}>
-								<Image src="img/dialup/dialupscreen.png" />
-							</Col>
-						</Row>
-					</Grid>
-				</Section>
+				{
+					this.state.content.map(
+						(section, idx) => <ProjectSection key={idx} content={section}/>
+					)
+				}
 			</div>
 		);
 	}
@@ -109,5 +95,28 @@ class HomePage extends React.Component {
 	}
 }
 
+class ProjectSection extends React.Component {
+	render() {
+		return (
+			<Section
+				bgColor={this.props.content.bgColor}
+				textColor={this.props.content.textColor}
+			>
+				<Grid fluid>
+					<Row>
+						<Col lg={4}>
+							<Title>{this.props.content.title.toUpperCase()}</Title>
+							<Body>{this.props.content.body}</Body>
+							<Role><b>Roles:</b> {this.props.content.roles}</Role>
+						</Col>
+						<Col lgOffset={1} lg={7}>
+							<Image src={this.props.content.media} />
+						</Col>
+					</Row>
+				</Grid>
+			</Section>
+		);
+	}
+}
 
 export default HomePage;
