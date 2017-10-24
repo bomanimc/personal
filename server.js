3945b37f0575a1d4194b94305ad46d829fbe335e
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2017-10-24 01:38:24 -0400

    Updates the server and adds a robots.txt file

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 const path = require('path');
 const Express = require('express');
+const robots = require('express-robots');
 
 const app = new Express();
 
 app.set('port', (process.env.PORT || 3000));
 app.set('env', (process.env.NODE_ENV || 'production'));
-app.use(Express.static(path.join(__dirname, 'src', 'static')));
+app.use(Express.static(path.join(__dirname, 'build')));
+app.use(robots(`${__dirname}/robots.txt`));
+
+// Google verification code
+// TODO: Add Verification Code
+// app.get('', function(req, res) {
+//     res.render('');
+// });
 
 // Universal routing and rendering handled by React & react-router
 // on the client-side.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src', 'static', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-// Start the server
+// start the server
 app.listen(app.get('port'), () => {
   console.log(`[${app.get('env')}] Express server listening on port ${app.get('port')}`);
 });
