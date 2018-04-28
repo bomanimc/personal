23e3a04d3eb1a872be4460c0f2ee0064b1fe3248
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2018-04-28 10:12:25 -0400

    Move data for projects and social media to a separate file

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -3,11 +3,11 @@
 
 import React from 'react';
 import styled from 'styled-components';
-import shortid from 'shortid';
 import Gallery from '../partials/LightboxGallery';
 import Project from '../partials/Project';
 import SocialLinksBar from '../partials/SocialLinksBar';
 import { Link, Body, TextContent } from '../commonComponents';
+import { ProjectContent, SocialLinks } from '../../constants';
 
 const skillAreaColors = {
   name: '#ffffff',
@@ -119,88 +119,7 @@ class HomePage extends React.Component {
   constructor() {
     super();
 
-    const content = [
-      {
-        id: shortid.generate(),
-        title: 'Shrumen Lumen',
-        tags: ['featured', 'software', 'art'],
-        body: `Interactive art installation made up of five glowing mushrooms
-        that react to the presence of people. Presented at Burning Man 2016
-        and [Meet D3 Festival](https://www.theluxediary.com/guide-meet-d3-2016/) in Dubai. On diplay at the [Smithsonian Museum](https://americanart.si.edu/exhibitions/burning-man) in 2018.`,
-        roles: `Lead Software Developer (First Showings),
-        Hardware-Software Integration.`,
-        media: 'img/shrumenlumen/shrumenlumen_main.png',
-        bgColor: '#919FB4',
-        textColor: '#FFF',
-        images: [
-          {
-            src: 'img/shrumenlumen/gallery/shrumen_sandy.jpg',
-          },
-          {
-            src: 'img/shrumenlumen/gallery/shrumen_solo.JPG',
-          },
-        ],
-      },
-      {
-        id: shortid.generate(),
-        title: 'Dial Up',
-        tags: ['featured', 'software', 'design'],
-        body: `Creative collective that creates music, videos, magazines, and
-        technology. Website featured on Brutalist Websites.`,
-        roles: 'Website Design/Develoment, DJ, Writer.',
-        media: 'img/dialup/dialup_main.png',
-        bgColor: '#FFF',
-        textColor: '#FF433E',
-        images: [
-          {
-            src: 'img/dialup/gallery/dialup_screen.png',
-          },
-        ],
-      },
-      {
-        id: shortid.generate(),
-        title: 'NegativeReel',
-        tags: ['software', 'art'],
-        body: `Projection mapping installation that displays negative thoughts
-        people have about themselves as an expression of vulnerability.`,
-        roles: 'Creative Lead, Projection Mapping, Software Development.',
-        media: 'img/negativereel/negativereel_main.png',
-        bgColor: '#FFF',
-        textColor: '#FF433E',
-        images: [
-          {
-            src: 'img/negativereel/gallery/negativereel_front.gif',
-          },
-        ],
-      },
-    ];
-
-    const socialLinks = [
-      {
-        name: 'Twitter',
-        link: 'https://twitter.com/bxmani',
-      },
-      {
-        name: 'Instagram',
-        link: 'https://www.instagram.com/bxmani/',
-      },
-      {
-        name: 'SoundCloud',
-        link: 'https://soundcloud.com/bxmani',
-      },
-      {
-        name: 'LinkedIn',
-        link: 'https://www.linkedin.com/in/bomanimc/',
-      },
-      {
-        name: 'GitHub',
-        link: 'https://github.com/bomanimc',
-      },
-    ];
-
     this.state = {
-      content,
-      socialLinks,
       showGallery: false,
       currentMedia: [],
     };
@@ -255,7 +174,7 @@ class HomePage extends React.Component {
           textColor="white"
           align="normal"
           sectionHeight="auto"
-          minHeight={`${(this.state.content.length * 250) + 200}px`}
+          minHeight={`${(ProjectContent.length * 250) + 200}px`}
           marginHorizontal="20px"
         >
           <ContentContainer horizontalCenter containerHeight="auto" marginTop="120px">
@@ -278,7 +197,7 @@ class HomePage extends React.Component {
           </ContentContainer>
           <ContentContainer horizontalCenter containerHeight="auto" marginTop="120px">
             {
-              this.state.content.map(
+              ProjectContent.map(
                 section =>
                   (<Project
                     key={section.id}
@@ -308,7 +227,7 @@ class HomePage extends React.Component {
             </TextContent>
           </Outro>
         </Section>
-        <SocialLinksBar links={this.state.socialLinks} />
+        <SocialLinksBar links={SocialLinks} />
         <Gallery
           isGalleryOpen={this.state.showGallery}
           onCloseHandler={() => this.setState({ showGallery: false })}
---
+++ src/constants.js
@@ -0,0 +1,81 @@
+import shortid from 'shortid';
+
+
+export const ProjectContent = [
+  {
+    id: shortid.generate(),
+    title: 'Shrumen Lumen',
+    tags: ['featured', 'software', 'art'],
+    body: `Interactive art installation made up of five glowing mushrooms
+    that react to the presence of people. Presented at Burning Man 2016
+    and [Meet D3 Festival](https://www.theluxediary.com/guide-meet-d3-2016/) in Dubai. On diplay at the [Smithsonian Museum](https://americanart.si.edu/exhibitions/burning-man) in 2018.`,
+    roles: `Lead Software Developer (First Showings),
+    Hardware-Software Integration.`,
+    media: 'img/shrumenlumen/shrumenlumen_main.png',
+    bgColor: '#919FB4',
+    textColor: '#FFF',
+    images: [
+      {
+        src: 'img/shrumenlumen/gallery/shrumen_sandy.jpg',
+      },
+      {
+        src: 'img/shrumenlumen/gallery/shrumen_solo.JPG',
+      },
+    ],
+  },
+  {
+    id: shortid.generate(),
+    title: 'Dial Up',
+    tags: ['featured', 'software', 'design'],
+    body: `Creative collective that creates music, videos, magazines, and
+    technology. Website featured on Brutalist Websites.`,
+    roles: 'Website Design/Develoment, DJ, Writer.',
+    media: 'img/dialup/dialup_main.png',
+    bgColor: '#FFF',
+    textColor: '#FF433E',
+    images: [
+      {
+        src: 'img/dialup/gallery/dialup_screen.png',
+      },
+    ],
+  },
+  {
+    id: shortid.generate(),
+    title: 'NegativeReel',
+    tags: ['software', 'art'],
+    body: `Projection mapping installation that displays negative thoughts
+    people have about themselves as an expression of vulnerability.`,
+    roles: 'Creative Lead, Projection Mapping, Software Development.',
+    media: 'img/negativereel/negativereel_main.png',
+    bgColor: '#FFF',
+    textColor: '#FF433E',
+    images: [
+      {
+        src: 'img/negativereel/gallery/negativereel_front.gif',
+      },
+    ],
+  },
+];
+
+export const SocialLinks = [
+  {
+    name: 'Twitter',
+    link: 'https://twitter.com/bxmani',
+  },
+  {
+    name: 'Instagram',
+    link: 'https://www.instagram.com/bxmani/',
+  },
+  {
+    name: 'SoundCloud',
+    link: 'https://soundcloud.com/bxmani',
+  },
+  {
+    name: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/bomanimc/',
+  },
+  {
+    name: 'GitHub',
+    link: 'https://github.com/bomanimc',
+  },
+];
