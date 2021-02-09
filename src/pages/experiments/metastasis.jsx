de4a8c9afe36ec28d9108b49bfc821c91a94c1f3
Bomani Oseni McClendon <bomanimc@gmail.com>
2021-02-09 14:23:32 -0500

    Test to ensure that the canvas is working

--- src/pages/experiments/metastasis.jsx
+++ src/pages/experiments/metastasis.jsx
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import Sketch from 'react-p5';
 import Layout from '../../components/layout';
 import { BaseAnimationPage } from '../../components/commonComponents';
 
-const Metastasis = () => {
-  const setup = (p5, canvasParentRef) => {
+class Metastasis extends Component {
+  x = 0;
+
+  y = 0;
+
+  setup = (p5, canvasParentRef) => {
     p5.createCanvas(window.innerWidth, window.innerHeight).parent(canvasParentRef);
   };
 
-  const draw = (p5) => {
-    p5.ellipse(400, 400, 400, 400);
+  draw = (p5) => {
+    p5.clear();
+    p5.ellipse(p5.mouseX, p5.mouseY, 400, 400);
   };
 
-  return (
-    <Layout showLinksBar={false}>
-      <BaseAnimationPage title="Metastasis">
-        <Metastasis.Container>
-          <Sketch setup={setup} draw={draw} />
-        </Metastasis.Container>
-      </BaseAnimationPage>
-    </Layout>
-  );
-};
+  render() {
+    return (
+      <Layout showLinksBar={false}>
+        <BaseAnimationPage title="Metastasis">
+          <Metastasis.Container>
+            <Sketch setup={this.setup} draw={this.draw} />
+          </Metastasis.Container>
+        </BaseAnimationPage>
+      </Layout>
+    );
+  }
+}
 
 Metastasis.Container = styled.div`
   position: absolute;
@@ -30,6 +37,7 @@ Metastasis.Container = styled.div`
   bottom: 0;
   left: 0;
   right: 0;
+  z-index: -1;
 `;
 
 export default Metastasis;
