987db4f65a2dd4509e333bfc7ec6470b1a8cc77f
Bomani Oseni McClendon <bomanimc@gmail.com>
2021-02-09 18:08:45 -0500

    Trying again

--- src/pages/experiments/vasoconstriction.jsx
+++ src/pages/experiments/vasoconstriction.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
-import loadable from '@loadable/component';
 import Layout from '../../components/layout';
 import { BaseAnimationPage } from '../../components/commonComponents';
-import VasoconstrictionSketch from '../../noc-sketches/vasoconstriction';
-
-// import { loadableP5 as P5Wrapper } from '../../components/loadable';
-const LoadableP5 = loadable(() => import('react-p5-wrapper'));
+import VasoconstrictionComponent from '../../components/vasoconstrictionComponent';
 
 const Vasoconstriction = () => (
   <Layout showLinksBar={false}>
     <BaseAnimationPage title="Vasoconstriction">
       <Vasoconstriction.Container>
-        <LoadableP5 sketch={VasoconstrictionSketch} />
+        <VasoconstrictionComponent />
       </Vasoconstriction.Container>
     </BaseAnimationPage>
   </Layout>
