ddf033252fe5c79860b908eb0e668265cdffd3b3
Bomani Oseni McClendon <bomanimc@gmail.com>
2021-02-09 14:15:31 -0500

    Add basic react-p5 frame to page

--- src/pages/experiments/metastasis.jsx
+++ src/pages/experiments/metastasis.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
+import Sketch from 'react-p5';
 import Layout from '../../components/layout';
 import { BaseAnimationPage } from '../../components/commonComponents';
 
 const Metastasis = () => {
+  const setup = (p5, canvasParentRef) => {
+    p5.createCanvas(window.innerWidth, window.innerHeight).parent(canvasParentRef);
+  };
+
+  const draw = (p5) => {
+    p5.ellipse(400, 400, 400, 400);
+  };
 
   return (
     <Layout showLinksBar={false}>
       <BaseAnimationPage title="Metastasis">
-        <p>Test</p>
+        <Metastasis.Container>
+          <Sketch setup={setup} draw={draw} />
+        </Metastasis.Container>
       </BaseAnimationPage>
     </Layout>
   );
 };
+
+Metastasis.Container = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+`;
 
 export default Metastasis;
