6af6ea7c1b711db160c78e5a1bf9a6dc5a48ebef
Bomani Oseni McClendon <bomanimc@gmail.com>
2021-02-09 17:54:20 -0500

    Fixing errors

--- src/components/loadable.js
+++ src/components/loadable.js
@@ -1,3 +1,4 @@
-import Loadable from "@loadable/component";
+/* eslint-disable import/prefer-default-export */
+import Loadable from '@loadable/component';
 
 export const loadableP5 = Loadable(() => import('react-p5-wrapper'));
