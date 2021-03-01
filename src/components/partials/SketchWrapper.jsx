5b552fc147851528a10549d0bd28bf6d1e2d8bdb
Bomani Oseni McClendon <bomanimc@gmail.com>
2021-02-28 21:52:21 -0500

    Attempt to get basic version of p5 integration added

---
+++ src/components/partials/SketchWrapper.jsx
@@ -0,0 +1,19 @@
+import React, { useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import p5 from 'p5';
+
+const SketchWrapper = (props) => {
+  const sketchRef = useRef();
+  useEffect(() => {
+    // eslint-disable-next-line no-new
+    new p5(props.sketch, sketchRef.current);
+  }, []);
+
+  return <div ref={sketchRef} />;
+};
+
+SketchWrapper.propTypes = {
+  sketch: PropTypes.func.isRequired,
+};
+
+export default SketchWrapper;
