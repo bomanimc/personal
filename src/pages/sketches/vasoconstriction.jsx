63ba8249e9ed55d9502812e2b8ee23d650bc659c
Bomani Oseni McClendon <bomanimc@gmail.com>
2021-02-28 22:40:44 -0500

    Tuning the useeffect pattern

--- src/pages/sketches/vasoconstriction.jsx
+++ src/pages/sketches/vasoconstriction.jsx
@@ -1,12 +1,18 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from '../../components/layout';
 import SketchWrapper from '../../components/partials/SketchWrapper';
 
 const Vasoconstriction = () => {
   const [sketch, setSketch] = useState();
 
-  import(`../../../scripts/sketches/test.js`).then((result) => setSketch(result));
+  // import('../../../scripts/sketches/test.js').then((result) => setSketch(result));
+  useEffect(() => {
+    const p5Sketch = require('../../../scripts/sketches/test.js');
+    console.log(p5Sketch);
+    console.log('Sketch Wrapper USE EFFECT RAN');
+    setSketch(p5Sketch);
+  });
 
   return sketch
     ? <Layout><SketchWrapper sketch={sketch.default} /></Layout>
