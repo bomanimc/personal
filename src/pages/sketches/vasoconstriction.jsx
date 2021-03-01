113b3f4d31d7e0a6392d5c297c8dac82c762c0ee
Bomani Oseni McClendon <bomanimc@gmail.com>
2021-02-28 22:32:37 -0500

    Remove checks from page file

--- src/pages/sketches/vasoconstriction.jsx
+++ src/pages/sketches/vasoconstriction.jsx
@@ -6,8 +6,6 @@ import SketchWrapper from '../../components/partials/SketchWrapper';
 const Vasoconstriction = () => {
   const [sketch, setSketch] = useState();
 
-  if (typeof window === 'undefined') return null;
-
   import(`../../../scripts/sketches/test.js`).then((result) => setSketch(result));
 
   return sketch
