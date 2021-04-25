cbf17aea31384c17272abe570250dc2970f7a195
Bomani Oseni McClendon <bomanimc@gmail.com>
2021-04-25 18:01:51 -0400

    Create an interesting gradient animation for the movers

--- src/sketches/forces.jsx
+++ src/sketches/forces.jsx
@@ -6,40 +6,14 @@ import Loadable from '@loadable/component';
 
 export const LoadableSketch = Loadable(() => import('react-p5'));
 
-class Attractor {
-  constructor(p5, x, y, m) {
-    this.p5 = p5;
-    this.pos = p5.createVector(x, y);
-    this.mass = m;
-    this.r = p5.sqrt(this.mass) * 2;
-  }
-
-  attract(mover) {
-    const { p5 } = this;
-    const force = p5.createVector(this.pos.x - mover.pos.x, this.pos.y - mover.pos.y);
-    const distanceSq = p5.constrain(force.magSq(), 100, 1000);
-    const G = 5;
-    const strength = (G * (this.mass * mover.mass)) / distanceSq;
-    force.setMag(strength);
-    mover.applyForce(force);
-  }
-
-  show() {
-    const { p5 } = this;
-    p5.noStroke();
-    p5.fill(255, 0, 100);
-    p5.ellipse(this.pos.x, this.pos.y, this.r * 2);
-  }
-}
-
 class Mover {
-  constructor(p5, x, y, m) {
+  constructor(p5, x, y, m, color) {
     this.p5 = p5;
     this.pos = p5.createVector(x, y);
-    this.vel = p5.createVector(p5.random(), p5.random());
-    this.vel.mult(5);
+    this.vel = p5.createVector(p5.random(0.1), p5.random(0.1));
     this.acc = p5.createVector(0, 0);
     this.mass = m;
+    this.color = color;
     this.r = p5.sqrt(this.mass) * 2;
   }
 
@@ -56,57 +30,108 @@ class Mover {
   }
 
   show() {
+    const { p5, color } = this;
+
+    const sinRange = p5.sin(0.0006 * p5.millis());
+    const diameter = 200 * sinRange;
+    const lerpedColor = p5.lerpColor(
+      p5.color(...[255, 82, 82, 10]),
+      p5.color(...color),
+      Math.abs(sinRange),
+    );
+    p5.stroke(lerpedColor);
+    p5.ellipse(
+      this.pos.x,
+      this.pos.y,
+      diameter,
+      diameter,
+    );
+  }
+}
+
+class Attractor {
+  constructor(p5, x, y, m) {
+    this.p5 = p5;
+    this.pos = p5.createVector(x, y);
+    this.mass = m;
+    this.r = p5.sqrt(this.mass) * 2;
+
+    this.movers = [];
+    this.initializeMovers();
+  }
+
+  initializeMovers() {
     const { p5 } = this;
-    p5.stroke(255);
-    p5.strokeWeight(2);
-    p5.fill(255, 100);
+    const color = [p5.random(255), p5.random(255), p5.random(255), 10];
+    const distanceFromPoint = 50;
+
+    for (let i = 0; i < 5; i += 1) {
+      const x = p5.random(this.x - distanceFromPoint, this.x + distanceFromPoint);
+      const y = p5.random(this.y - distanceFromPoint, this.y + distanceFromPoint);
+      const m = p5.random(10, 50);
+      this.movers.push(new Mover(p5, x, y, m, color));
+    }
+  }
+
+  attract(mover) {
+    const { p5 } = this;
+    const force = p5.createVector(this.pos.x - mover.pos.x, this.pos.y - mover.pos.y);
+    const distanceSq = p5.constrain(force.magSq(), 100, 1000);
+    const G = 2;
+    const strength = (G * (this.mass * mover.mass)) / distanceSq;
+    force.setMag(strength);
+    mover.applyForce(force);
+  }
+
+  show() {
+    const { p5 } = this;
+    p5.noFill();
     p5.ellipse(this.pos.x, this.pos.y, this.r * 2);
+  }
+
+  updateMovers() {
+    this.movers.forEach((mover) => {
+      mover.update();
+      mover.show();
+      this.attract(mover);
+    });
   }
 }
 
 class Sketch extends Component {
-  movers = [];
-
   attractors = [];
 
   setup = (p5, canvasParentRef) => {
     const { width, height } = this.getCanvasSizing();
     p5.createCanvas(width, height).parent(canvasParentRef);
 
-    for (let i = 0; i < 10; i += 1) {
-      const x = p5.random(width);
-      const y = p5.random(height);
-      const m = p5.random(50, 150);
-      this.movers[i] = new Mover(p5, x, y, m);
-    }
-
     p5.push();
     p5.translate(width / 2, height / 2);
     const radius = 300;
     let angle = 0;
-    for (let i = 0; i < 6; i += 1) {
+    const numAttractors = 12;
+    for (let i = 0; i < numAttractors; i += 1) {
       const x = radius * p5.sin(angle);
       const y = radius * p5.cos(angle);
-      this.attractors.push(new Attractor(p5, x, y, 100));
-      angle += p5.TWO_PI / 6;
+      this.attractors.push(new Attractor(p5, x, y, 10));
+      angle += p5.TWO_PI / numAttractors;
     }
     p5.pop();
 
     p5.background(0);
+    p5.noFill();
+    p5.blendMode(p5.ADD);
   };
 
   draw = (p5) => {
-    p5.background(0);
+    p5.background(0, 0, 0, 255);
     p5.push();
     p5.translate(p5.width / 2, p5.height / 2);
 
-    this.movers.forEach((mover) => {
-      mover.update();
-      mover.show();
-      this.attractors.map((attractor) => attractor.attract(mover));
+    this.attractors.map((attractor) => {
+      attractor.updateMovers();
+      attractor.show();
     });
-
-    this.attractors.map((attractor) => attractor.show());
 
     p5.pop();
   };
