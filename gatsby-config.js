af6ec8f70ff7849c6200393dc6649d285c1c485b
Bomani Oseni McClendon <bomanimc@gmail.com>
2024-06-23 20:01:31 -0400

    Update manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,17 +30,15 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: 'BOMANI',
+        short_name: 'BOMANI',
+        start_url: '/',
+        background_color: '#000',
+        theme_color: '#000',
+        display: 'minimal-ui',
+        icon: 'src/images/drip.png', // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-styled-components`
