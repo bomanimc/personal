675b7dae8aa8141b7cad1327fc7e624474ddaadf
Bomani Oseni McClendon <bomanimc@gmail.com>
2026-02-15 18:55:05 -0500

    Replace repo with empty starter

--- sanity/sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'hhel7lt4',
-    dataset: 'production'
-  }
-})
--- sanity/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-import {projectType} from './project'
-import {personInfoType} from './personInfo'
-
-export const schemaTypes = [
-  projectType,
-  personInfoType,
-];
