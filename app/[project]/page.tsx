3f33a60d8770b921801fffc3c9cdf8eeebfdb626
Bomani Oseni McClendon <bomanimc@gmail.com>
2026-04-06 01:55:32 +0930

    404 on invalid project routes

--- app/[project]/page.tsx
+++ app/[project]/page.tsx
@@ -14,6 +14,7 @@ import {
 } from '../../components/CommonComponents';
 import ProjectBodyContent from "./projectBodyContent";
 import { Metadata } from "next";
+import { notFound } from "next/navigation";
 interface ProjectParams {
   project: string;
 }
@@ -39,7 +40,7 @@ export default async function ProjectPage({ params }: Props) {
 
   // TODO: Understand why this is possible null
   if (!fetchedProject) {
-    return;
+    notFound();
   }
 
   const { title, year, tools, role, site, content, otherMedia } =
